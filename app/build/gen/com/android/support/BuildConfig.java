a552b20beb18558f88db75f0c83060bcfa6c54ea
Duong Dev <vvvjdzghyt@gmail.com>
2026-08-23 12:23:07 +0700

    Add files via upload

---
+++ app/build/gen/com/android/support/BuildConfig.java
@@ -0,0 +1,10 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.android.support;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+    public final static String APPLICATION_ID = "com.android.support";
+    public final static String BUILD_TYPE = "debug";
+    public final static long VERSION_CODE = 1;
+    public final static String VERSION_NAME = "3.2";
+}
